25b5f0b7dca33d59e709ff485678543edd4431f0
Randy Russell <ransolo@me.com>
2014-03-28 21:28:43 -0500

    got it now

--- app/app.js
+++ app/app.js
@@ -28,7 +28,7 @@ app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(express.cookieParser());
 app.use(express.session({
-  store : new RedisStore({host: '192.168.1.134', port: 6379}),
+  store : new RedisStore({host: 'localhost', port: 6379}),
   secret: 'change-this-to-a-super-secret-message',
   cookie: { maxAge: 24 * 60 * 60 * 1000 }
 }));
--- app/lib/init-mongo.js
+++ app/lib/init-mongo.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var MongoClient = require('mongodb').MongoClient;
-var mongoUrl = 'mongodb://192.168.1.134/' + process.env.DBNAME;
+var mongoUrl = 'mongodb://localhost/' + process.env.DBNAME;
 var initialized = false;
 
 exports.connect = function(req, res, next){
